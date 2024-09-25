2021f9d4081044017b753f004744d2f314a06b0e
jobayermannan <jobayermannan777@gmail.com>
2024-09-25 16:03:27 +0600

    Installing Shadcn with pair extraordinarie.
    Co-authored-by:coding-struggles <oxobayer777@gmail.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import * as globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
